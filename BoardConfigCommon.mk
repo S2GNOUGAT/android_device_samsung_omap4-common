3eda2a787b1f0bbc01184cd9f6ea6e9d36f7da2a
xdaamg <goetz@gametool.de>
2015-12-08 13:51:05 +0100

    remove sepolicy union
    Change-Id: I0d93833f04c3166bb284d9ea9dc208d9ec6d1173

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,18 +68,6 @@ BOARD_USE_CUSTOM_LIBION := true
 BOARD_SEPOLICY_DIRS += \
     device/samsung/omap4-common/sepolicy
 
-BOARD_SEPOLICY_UNION += \
-    bluetooth.te \
-    device.te \
-    domain.te \
-    file.te \
-    file_contexts \
-    init.te \
-    pvrsrvinit.te \
-    radio.te \
-    rild.te \
-    wpa_supplicant.te
-
 # TI Enhancement Settings (Part 1)
 OMAP_ENHANCEMENT := true
 #OMAP_ENHANCEMENT_BURST_CAPTURE := true
