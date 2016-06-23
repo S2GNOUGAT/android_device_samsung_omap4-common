7065fafc44e86942e0b3a626acfdc30ad72f7aef
xdaamg <goetz@gametool.de>
2016-06-23 15:58:50 +0200

    omap4-next: update omap4-common
    Change-Id: I3c523e8d14f2e1c1c657e755bb5fb7d6cdf70ecf

--- common.mk
+++ common.mk
@@ -14,20 +14,18 @@
 # limitations under the License.
 #
 
+# Include common omap4 makefile
+$(call inherit-product, hardware/ti/omap4/omap4.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 COMMON_PATH := device/samsung/omap4-common
 
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 
 # Omap4 Packages
 PRODUCT_PACKAGES += \
-    libedid \
-    libion_ti \
-    libstagefrighthw \
-    smc_pa_ctrl \
-    tf_daemon \
-    libcorkscrew \
-    pvrsrvinit \
-    libPVRScopeServices.so
+    libedid
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
@@ -41,18 +39,28 @@ PRODUCT_PACKAGES += \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-    static_busybox \
     make_ext4fs \
     setup_fs
 
+# RIL
+PRODUCT_PACKAGES += \
+    libsecril-client \
+    libxml2
+
+# Misc S2
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory \
+    SamsungServiceMode
+    
 PRODUCT_PROPERTY_OVERRIDES += \
-    com.ti.omap_enhancement=true \
-    omap.enhancement=true \
     ro.telephony.ril_class=SamsungOmap4RIL \
     ro.telephony.call_ring.multiple=false \
     ro.telephony.call_ring.delay=3000 \
     ro.bq.gpu_to_cpu_unsupported=1 \
     camera2.portability.force_api=1
+#    com.ti.omap_enhancement=true \
+#    omap.enhancement=true \
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -64,3 +72,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Include non-opensource parts
 $(call inherit-product, vendor/samsung/omap4-common/common-vendor.mk)
+
